473f7573592591224bf23a11d9a5ad9ee15d0c84
dilipMaharjan <dilipmaharjann@gmail.com>
2018-02-27 16:06:03 -0500

    authentication

--- app/settings.py
+++ app/settings.py
@@ -38,9 +38,16 @@ INSTALLED_APPS = [
 
     'rest_framework',
     'rest_framework.authtoken',
+
     'rest_auth',
+    'django.contrib.sites',
+    'allauth',
+    'allauth.account',
+    'rest_auth.registration',
+
     'api',
 ]
+SITE_ID = 1
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
